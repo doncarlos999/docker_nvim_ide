3386d72f8a61bd2dee41b9ed4568eb131817979c
carl <carl@ip-172-31-96-234.ec2.internal>
2021-03-12 09:32:45 +0000

    removed unused scripts

--- 2.finish_setup.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-cat ~/.zshrc ~/amazon_EC2_setup/append_zshrc > tmp
-mv tmp ~/.zshrc
-sed -i 's/robbyrussell/agnoster/g' ~/.zshrc
-sed -i 's/plugins=(git)/plugins=(git command-not-found common-aliases debian dircycle dirhistory history history-substring-search colored-man-pages)/g' ~/.zshrc 
-source ~/.zshrc
